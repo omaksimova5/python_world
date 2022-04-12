77cd7f71082f9473ce13e1ef29b3653edb92d177
omaksimova5 <87267202+omaksimova5@users.noreply.github.com>
2022-04-12 22:25:03 +0900

    Add files via upload

---
+++ dz3.py
@@ -0,0 +1,7 @@
+n = int(input("Введите число: "))
+number = str(n)
+t1 = number + number
+t2 = number + number + number
+result = n + int(t1) + int(t2)
+print("Результат равен:", result)
+
---
+++ dz4.py
@@ -0,0 +1,8 @@
+num = int(input ('Введите число :'))
+max = num%10
+num = num//10
+while num > 0:
+    if num%10 > max:
+        max = num%10
+    num = num//10
+print(max)
